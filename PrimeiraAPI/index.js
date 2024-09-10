3cd8a7ef508f77734d27fb828caecd661d89c144
pedro <151687035+pedrokunrath@users.noreply.github.com>
2024-09-09 21:35:37 -0300

    first commit

---
+++ PrimeiraAPI/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.listen(8080,() => {
+  console.log("o servidor esta rodando na porta 8080")   
+});
+
+app.get('/', (req, res) => {
+    try {
+        throw new Error("Not implemented")
+        res.status(200).send("ok");
+    }catch(erro) {
+        res.status(500).send(erro.message);
+    }
+});
+
+app.post('/cadastar usuario', (req, res) => {
+    const { nome, email, senha } = req.body;
+});
