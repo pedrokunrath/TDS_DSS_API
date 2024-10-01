b66f6912118bd1ef8cf64163c04f6c1261fc2165
pedrokunrath <pedro10kunrath@gmail.com>
2024-10-01 19:09:38 -0300

    .

--- API_MVC/server.js
+++ API_MVC/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const clienteController = require('./controller/cliente.controller');
 
 const app = express();
-const port = 3000;
+const port = 3005;
 
 app.use(bodyParser.json());
 
