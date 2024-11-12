4e1df8077c2824b25bba0c6a192a4568513efb63
pedrokunrath <pedro10kunrath@gmail.com>
2024-11-11 21:22:14 -0300

    commit refactor code

---
+++ API_TRANSACTION/routes/index.routes.js
@@ -0,0 +1,7 @@
+const routes = require("express").Router();
+const transactionRouter = require("./transaction.router");
+const userRouter = require("./user.router");
+
+routes.use("/transactions", transactionRouter);
+routes.use("/user", userRouter);
+module.exports = routes;
